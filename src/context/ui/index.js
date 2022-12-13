0ee9fa4e3b31514995ded4d4917c16dad4cd494a
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-12 19:12:54 -0800

    add user state to ui context

--- src/context/ui/index.js
+++ src/context/ui/index.js
@@ -1,15 +1,15 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState } from 'react';
 
 export const UIContext = createContext({});
 export const useUIContext = () => useContext(UIContext);
 
 export const UIProvider = ({ children }) => {
-
   const [showSearchBox, setShowSearchBox] = useState(false);
   const [cart, setCart] = useState([]);
   const [showCart, setShowCart] = useState(false);
   const [favorites, setFavorites] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
+  const [user, setUser] = useState({});
 
   const value = {
     showSearchBox,
@@ -22,7 +22,9 @@ export const UIProvider = ({ children }) => {
     setFavorites,
     showFavorites,
     setShowFavorites,
+    user,
+    setUser,
   };
 
-  return <UIContext.Provider value={value}>{children}</UIContext.Provider>
-}
+  return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
+};
