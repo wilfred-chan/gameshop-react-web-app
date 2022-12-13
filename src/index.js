76c5ad4c667ecf9090321b987e585a1ac9466a1e
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-13 08:02:01 -0800

    add router for register page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/login';
 import { UserContextProvider } from './context/user';
+import RegisterPage from './components/register';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +16,7 @@ root.render(
         <Routes>
           <Route path="/*" element={<App />} />
           <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
         </Routes>
       </UserContextProvider>
     </BrowserRouter>
