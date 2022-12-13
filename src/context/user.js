474bfcbf22641c3922fce70e27922b7dc17d4ae5
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-13 07:35:46 -0800

    creat a new context for user state management

---
+++ src/context/user.js
@@ -0,0 +1,14 @@
+import { createContext, useState } from 'react';
+
+export const UserContext = createContext({});
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState({ loggedIn: false });
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
