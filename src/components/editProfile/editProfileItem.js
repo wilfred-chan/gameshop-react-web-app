41f0f54431c4e427794546c0b136c021848125e8
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-14 08:19:54 -0800

    bind API with EditProfile page

--- src/components/editProfile/editProfileItem.js
+++ src/components/editProfile/editProfileItem.js
@@ -1,45 +1,99 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import { UserContext } from '../../context/user';
+import { Link, useNavigate } from 'react-router-dom';
+
+const divStyle = {
+  width: 100,
+};
+
+const BASE_URL = 'https://gameshop.herokuapp.com/api/users/';
 
 const EditProfileItem = ({ profile }) => {
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const [values, setValues] = useState(user);
 
-    return (
-        <>
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-            <form className="pt-5 form-width">
-                <div>
-                    <span>User Name</span>
-                    <input className="form-control"
-                        required
-                        id="firstname"
-                        type="text"
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      firstname: e.target.firstname.value,
+      bio: e.target.bio.value,
+      location: e.target.location.value,
+    };
+    axios
+      .put(BASE_URL + user.username, data)
+      .then((res) => {
+        console.log('User Profile Updated');
+        setUser({ ...user, ...data });
+        navigate('/profile');
+      })
+      .catch((err) => {
+        console.log(err);
+        window.alert(err.message);
+      });
+  };
 
-                    />
-                </div>
+  return (
+    <>
+      <form className="pt-5 form-width" onSubmit={handleSubmit}>
+        <div>
+          <span>First Name</span>
+          <input
+            className="form-control"
+            required
+            name="firstname"
+            type="text"
+            value={values.firstname}
+            onChange={handleChange}
+          />
+          <span>Last Name</span>
+          <input
+            className="form-control"
+            required
+            name="lastname"
+            type="text"
+            value={values.lastname}
+            onChange={handleChange}
+          />
+        </div>
 
-                <div>
-                    <span>Bio</span>
-                    <input className="form-control"
-                        type="text"
-                    />
-                </div>
+        <div>
+          <span>Bio</span>
+          <input
+            className="form-control"
+            type="text"
+            name="bio"
+            value={values.bio}
+            onChange={handleChange}
+          />
+        </div>
 
-                <div>
-                    <span>Location</span>
-                    <input
-                        type="text"
-                        className="form-control"
-                    />
-                </div>
-            </form>
-
-            <div className="position-relative btn-pos align-self-center my-2 pt-2">
-                <button className="btn btn-secondary">
-                        Save
-                </ button>
-
-            </div>
-
-        </>
-    )
-}
-export default EditProfileItem
+        <div>
+          <span>Location</span>
+          <input
+            type="text"
+            className="form-control"
+            name="location"
+            value={values.location}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mx-auto" style={divStyle}>
+          <button
+            className="btn btn-secondary mt-3"
+            type="submit"
+            value="Submit"
+            style={divStyle}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+export default EditProfileItem;
