4fa9377ba6d4f43291a792d4bd9b62f73407cb38
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-13 22:18:20 -0800

    fix dep error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,8 +10,8 @@ import RegisterPage from './components/register';
 import ProfileScreen from './components/profile';
 import EditProfile from './components/editProfile/CustomerCardEditProfile'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserContextProvider>
@@ -24,8 +24,10 @@ root.render(
         </Routes>
       </UserContextProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+ document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
