a5d6577c04d92645b6d20086991373678609d6f7
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-14 12:43:01 -0800

    improve admin dashboard

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -1,10 +1,9 @@
-import axios from "axios";
-import {useEffect, useState} from "react";
-import MaterialTable from 'material-table'
-import {Box, ThemeProvider, createTheme} from "@mui/material";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import MaterialTable from 'material-table';
+import { Box, ThemeProvider, createTheme, Button } from '@mui/material';
 
-
-const BASE_URL = "https://gameshop.herokuapp.com/api/";
+const BASE_URL = 'https://gameshop.herokuapp.com/api/';
 
 export default function Dashboard() {
   const [products, setProducts] = useState([]);
@@ -14,49 +13,41 @@ export default function Dashboard() {
     const getProducts = async () => {
       const response = await axios.get(BASE_URL + 'games');
       setProducts(response.data);
-    }
+    };
     try {
       getProducts();
-      console.log(products);
+      // console.log(products);
     } catch (error) {
       console.log(error);
     }
-  }, [])
+  }, []);
 
-  return(
-      <ThemeProvider theme={defaultMaterialTheme}>
-        <Box
-            m="40px 0 0 0"
-            height="75vh"
-        >
-          <MaterialTable
-              columns={[
 
-                { title: "Game Id", field: "game_id" },
-                { title: "Name", field: "title" },
-                { title: "Rating", field: "rating" },
-                { title: "Release Date", field: "released" },
-                { title: "Price", field: "price" },
-                { title: "Description", field: "description" },
-                {
-                  title: "Cover image",
-                  field: "cover_image",
-                  render: (rowData) => (
-                      <img
-                          style={{ height: 36, borderRadius: "50%" }}
-                          src={rowData.cover_image}
-                      />
-                  ),
-                },
-              ]}
-              data={products}
-              options={{
-                search: true
-              }}
-          />
-        </Box>
-      </ThemeProvider>
-
+  return (
+    <ThemeProvider theme={defaultMaterialTheme}>
+      <Box m="40px 0 0 0" height="75vh">
+        <MaterialTable
+          columns={[
+            { title: 'Game Id', field: 'game_id' },
+            { title: 'Price', field: 'price' },
+            {
+              title: 'Cover image',
+              field: 'cover_image',
+              render: (rowData) => (
+                <img
+                  style={{ height: 36, borderRadius: '50%' }}
+                  src={rowData.cover_image}
+                />
+              ),
+            },
+          ]}
+          data={products}
+          title="GameShop Stock"
+          options={{
+            search: false,
+          }}
+        />
+      </Box>
+    </ThemeProvider>
   );
-
 }
