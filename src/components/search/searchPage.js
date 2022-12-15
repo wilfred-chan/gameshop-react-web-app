3302e7d368bff1858d19978d2d880e247e2bbf42
JiarongF <jiaronf1@uci.edu>
2022-12-14 21:39:38 -0800

    change ui of search page

--- src/components/search/searchPage.js
+++ src/components/search/searchPage.js
@@ -3,20 +3,27 @@ import {
   Typography,
   Box,
   TextField,
-  FormControlLabel,
-  Checkbox,
-  Button,
-  Grid,
+  Grid, ListItemButton, ListItemIcon, IconButton,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import HomeIcon from '@mui/icons-material/Home';
+
 
 import SearchResultItem from './searchResultItem';
+import {Colors} from "../../styles/theme";
+import {
+  ActionIconsContainer,
+  HeaderBar,
+  HeaderContainer,
+  NavLinks
+} from "../../styles/header";
+import SearchIcon from "@mui/icons-material/Search";
+
 
 
 const BASE_URL = "https://gameshop.herokuapp.com/api/search/";
-
 
 export default function SearchPage() {
   const [results, setResults] = useState([]);
@@ -30,41 +37,88 @@ export default function SearchPage() {
   };
 
   return (
-    <>
-      <Container component="main" maxWidth="xs">
-        <Link to="/">Back to home</Link>
-        <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
-          Search Games
-        </Typography>
-        <Container
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            id="input"
-            label="Type a game name"
-            name="input"
-            autoFocus
-          />
-          <Button type="submit" variant="contained" sx={{ mt: 3, mx: 3 }}>
-            Search
-          </Button>
-          <Grid container>
-            <Grid item xs></Grid>
-          </Grid>
-        </Container>
-      </Container>
-      <Container component="main" maxWidth="xs">
+      <>
+        <HeaderContainer>
+          <HeaderBar>GameShop</HeaderBar>
+          <ActionIconsContainer>
+            <NavLinks type="row">
+              <ListItemButton
+                  sx={{
+                    justifyContent: "center",
+                  }}
+              >
+                <ListItemIcon
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: Colors.secondary,
+                    }}
+                >
+                  <Link to="/" >
+                    <HomeIcon sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      color: Colors.secondary,
+                    }}/>
+                  </Link>
+                </ListItemIcon>
+              </ListItemButton>
 
-        {results.length > 0 && <Typography component="h1" variant="h5" sx={{ mt: 1 }}>
-          Search Results
-        </Typography>}
-        {results.map((result) => (
-          <SearchResultItem game={result} key={result.game_id}/>
-        ))}
-      </Container>
-    </>
+            </NavLinks>
+          </ActionIconsContainer>
+        </HeaderContainer>
+        <Container component="main" maxWidth="xs">
+          <Box display = "flex" justifyContent={"center"} sx = {{p: 4}}>
+            <Typography variant = "h5" sx ={{color: Colors.primary}}>Search Games</Typography>
+          </Box>
+
+          <Container
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx = {{marginLeft: 10, marginBottom:1}}>
+            <TextField
+
+                id="input"
+                color = "secondary"
+                placeholder="Type a game name"
+                name="input"
+                variant="standard"
+                autoFocus
+            />
+            <IconButton type="submit">
+              <SearchIcon sx={{ fontSize: { xs: '3rem', md: "2rem" }, marginTop: 1}} color="secondary" />
+            </IconButton>
+
+            <Grid container>
+              <Grid item xs></Grid>
+            </Grid>
+          </Container>
+        </Container>
+        <Container component="main">
+
+          {results.length > 0 && <Box display = "flex" justifyContent={"center"} sx = {{p: 4}}>
+            <Typography variant = "h5" sx ={{color: Colors.primary}}>Search Results</Typography>
+          </Box>}
+          <Container>
+            <Grid
+                container
+                spacing={{ xs: 2, md: 3 }}
+                justifyContent="center"
+                sx={{ margin: `20px 4px 10px 4px` }}
+                columns={{ xs: 4, sm: 8, md: 12 }}
+            >
+
+              {results.map((result) => (
+                  <Grid item key={result.game_id} xs={2} sm={4} md={4} display="flex" flexDirection={'column'} alignItems="center">
+                  <SearchResultItem game={result} key={result.game_id}/>
+                  </Grid>
+              ))}
+
+            </Grid>
+          </Container>
+
+        </Container >
+      </>
   );
 }
--- src/components/search/searchResultItem.js
+++ src/components/search/searchResultItem.js
@@ -1,54 +1,35 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Item from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
+import {ProductMetaWrapper
+} from "../../styles/products";
+import InventoryIcon from '@mui/icons-material/Inventory';
+import {Colors} from "../../styles/theme";
 
-const StockButton = ({ game }) => {
-  if (game.in_stock) {
-    return <Link to={`/product/${game.game_id}`}><Button variant="contained">In Stock</Button></Link>;
-  } else {
-    return (
-      <Button variant="contained" disabled>
-        OUT OF STOCK
-      </Button>
-    );
-  }
-};
+
 
 export default function SearchResultItem({ game }) {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={9}>
-        <Item>
-          <Box
-            sx={{
-              bgcolor: 'background.paper',
-              boxShadow: 1,
-              borderRadius: 2,
-              p: 2,
-              minWidth: 300,
-              my: 2,
-            }}>
-            <Typography variant="h5" gutterBottom>
-              {game.title}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              Rating: {game.rating}/5
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              Release Date: {game.released}
-            </Typography>
-          </Box>
-        </Item>
-      </Grid>
-      <Grid item xs={3}>
-        <Item>
-          <StockButton game={game} />
-        </Item>
-      </Grid>
-    </Grid>
+
+        <ProductMetaWrapper sx ={{background: Colors.secondary, color: Colors.white}}>
+          <Typography variant= "h5" lineHeight={2}>
+            {game.title}
+          </Typography>
+          <Typography variant="body1">
+            Rating: {game.rating}/5
+          </Typography>
+          <Typography variant="body1">
+            Release Date: {game.released}
+          </Typography>
+          {game.in_stock && <Link to={`/product/${game.game_id}`}>
+            <InventoryIcon sx={{ color: game.in_stock? Colors.danger : Colors.dim_grey}}/>
+          </Link>}
+          {!game.in_stock && <Link to={`/product/${game.game_id}`}>
+            <InventoryIcon sx={{ color: game.in_stock? Colors.danger : Colors.dim_grey}}/>
+          </Link>}
+
+        </ProductMetaWrapper>
+
+
   );
 }
