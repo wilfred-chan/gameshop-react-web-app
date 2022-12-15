5a7f3e0a035a394fc2fee0a8d0873d0117459025
Z. Chen <69495364+wilfred-chan@users.noreply.github.com>
2022-12-15 08:29:43 -0800

    add OUT OF STOCK info to search result

--- src/components/search/searchResultItem.js
+++ src/components/search/searchResultItem.js
@@ -1,35 +1,35 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
-import {ProductMetaWrapper
-} from "../../styles/products";
+import { ProductMetaWrapper } from '../../styles/products';
 import InventoryIcon from '@mui/icons-material/Inventory';
-import {Colors} from "../../styles/theme";
-
-
+import { Colors } from '../../styles/theme';
+import { Container } from '@mui/system';
 
 export default function SearchResultItem({ game }) {
   return (
-
-        <ProductMetaWrapper sx ={{background: Colors.secondary, color: Colors.white}}>
-          <Typography variant= "h5" lineHeight={2}>
-            {game.title}
-          </Typography>
-          <Typography variant="body1">
-            Rating: {game.rating}/5
-          </Typography>
-          <Typography variant="body1">
-            Release Date: {game.released}
-          </Typography>
-          {game.in_stock && <Link to={`/product/${game.game_id}`}>
-            <InventoryIcon sx={{ color: game.in_stock? Colors.danger : Colors.dim_grey}}/>
-          </Link>}
-          {!game.in_stock && <Link to={`/product/${game.game_id}`}>
-            <InventoryIcon sx={{ color: game.in_stock? Colors.danger : Colors.dim_grey}}/>
-          </Link>}
-
-        </ProductMetaWrapper>
-
-
+    <ProductMetaWrapper
+      sx={{ background: Colors.secondary, color: Colors.white }}>
+      <Typography variant="h5" lineHeight={2}>
+        {game.title}
+      </Typography>
+      <Typography variant="body1">Rating: {game.rating}/5</Typography>
+      <Typography variant="body1">Release Date: {game.released}</Typography>
+      {game.in_stock && (
+        <Link to={`/product/${game.game_id}`}>
+          <InventoryIcon
+            sx={{ color: game.in_stock ? Colors.danger : Colors.dim_grey }}
+          />
+        </Link>
+      )}
+      {!game.in_stock && (
+        <>
+          <InventoryIcon
+            sx={{ color: game.in_stock ? Colors.danger : Colors.dim_grey }}
+          />
+          <span> OUT OF STOCK</span>
+        </>
+      )}
+    </ProductMetaWrapper>
   );
 }
